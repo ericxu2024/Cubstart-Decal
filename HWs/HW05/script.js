9177543c742fb33e9a0817a5ed319477622b53be
Eric Xu <ericxu2024@berkeley.edu>
2023-03-18 22:25:27 -0700

    finished filling in code but need to debug

--- HWs/HW05/script.js
+++ HWs/HW05/script.js
@@ -1,12 +1,12 @@
 // Ddoski: I lost my API key! Could I use yours? (CLUE #1: See more info in the homework spec.)
-apiKey = /* YOUR API KEY HERE */;
+apiKey = "&appid=f9a7c9974170cfd955391620fe0cd0e7";
 
 geocodingUrl = "http://api.openweathermap.org/geo/1.0/direct?q="; // Ddoski: This is the API we use to convert city names to coordinates!
 weatherUrl = "https://api.openweathermap.org/data/2.5/weather?"; // Ddoski: This is the API we use to convert coordinates into weather data!
 
 submitButton = document.getElementById("submit");
 // Ddoski: I don't remember what I put here, but I know it selects the input element with the id cityName.
-cityName = /* YOUR CODE HERE */
+cityName = document.getElementById("cityName");
 mainWeather = document.getElementById("mainWeather");
 weatherDescription = document.getElementById("weatherDescription");
 
@@ -38,14 +38,14 @@ async function getLatLon(city) {
   // Ddoski: Let's create the url we call the API on here. 
   // Be sure to use a url provided above, and the city name provided by the user.
   // (CLUE #2: See more info in the homework spec.)
-  let formattedUrl = /* YOUR CODE HERE */
-  let data = await apiCall(formattedUrl)
+  let formattedUrl = geocodingUrl + cityName + apiKey;
+  let data = await apiCall(formattedUrl);
 
   // Ddoski: We're returning a Javascript object here! 
   // Which part of the data variable do we extract for the lat and lon?
   return {
-    "lat": /* YOUR CODE HERE */,
-    "lon": /* YOUR CODE HERE */
+    "lat": data[0].lat,
+    "lon": data[0].lon,
   }
 }
 
@@ -54,9 +54,11 @@ async function getLatLon(city) {
 // (CLUE #3: See more info in the homework spec.)
 async function getWeather(lat, lon) {
   /* YOUR CODE HERE */
+  let formattedUrl = weatherUrl + "lat=" + lat + + "lon=" + lon + apiKey;
+  let data = await apiCall(formattedUrl);
   return {
-    "main": /* YOUR CODE HERE */,
-    "description": /* YOUR CODE HERE */
+    "main": data.weather[0].main,
+    "description": data.weather[0].description,
   }
 }
 
@@ -68,12 +70,13 @@ async function setWeatherDescription(city) {
   // Ddoski: Take a look at the format 
   // of what getLatLon returns above. How would we extract the lat and lon from this?
   // (CLUE #4: See more info in the homework spec.)
-  const lat = /* YOUR CODE HERE */
-  const lon = /* YOUR CODE HERE */
+  const lat = coordData.lat;
+  const lon = coordData.lon;
 
   let weatherData = await getWeather(lat, lon)
   
   // Ddoski: Same thing here, but we're trying to set mainWeather and weatherDescription's innerHTML 
   // to the relevant values in weatherData.
-  /* YOUR CODE HERE */
+  mainWeather.innerHTML = weatherData.main;
+  weatherDescription.innerHTML = weatherData.description;
 }
